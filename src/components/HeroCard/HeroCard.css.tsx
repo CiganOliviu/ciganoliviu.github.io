a4e8e46405741e8d0f41d01aa528b0a834cbb99d
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-15 17:06:18 +0300

    COD-1 | Updates

--- src/components/HeroCard/HeroCard.css.tsx
+++ src/components/HeroCard/HeroCard.css.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { AppTheme } from "@components/utils/cssMedia";
 
 export const HeroCardBox = styled.div`
     background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://res.cloudinary.com/practicaldev/image/fetch/s--ivd9kMB---/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://images.unsplash.com/photo-1569012871812-f38ee64cd54c%3Fixid%3DMXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%253D%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1650%26q%3D80') no-repeat center center fixed;
@@ -12,7 +13,7 @@ export const HeroCardContainer = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    color: #fff;
+    color: ${AppTheme.white};
     text-align: center;
 `;
 
@@ -34,9 +35,9 @@ export const HeroCardContinuityText = styled.div`
 `;
 
 export const HeroCardContactButton = styled.div`
-    color: #20c997;
+    color: ${AppTheme.lightGreen};
     background: transparent;
-    border: solid #20c997;
+    border: solid ${AppTheme.lightGreen};
     border-radius: 2rem;
     padding: 0.8rem 1.8rem 0.8rem 1.8rem; 
     text-align: center; 
@@ -48,7 +49,7 @@ export const HeroCardContactButton = styled.div`
     transition: 0.5s ease;
     
     :hover {
-        background: #20c997;
-        color: white;
+        background: ${AppTheme.lightGreen};
+        color: ${AppTheme.white};
     }
 `
