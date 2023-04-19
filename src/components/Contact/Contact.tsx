ca93592461cc079264c3a35fb8f700ab9eb8b893
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-19 16:25:50 +0300

    COD-15 | Updates

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,87 +2,52 @@ import { TextContainer } from "@components/components/TextContainer/TextContaine
 import {
     ContactContainerBox,
     ContactContainerForm,
-    ContactEssentialFlex, ContactForm, ContactFormErrorMessage,
+    ContactEssentialFlex, ContactForm,
     ContactFormInput,
     ContactFormText, ContactFormTextArea
 } from "@components/components/Contact/Contact.css";
 import { Separator, SimpleText } from "@components/components/Resume/Resume.css";
 import { GeneralButton } from "@components/components/HeroCard/HeroCard.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { isValidEmail } from "@components/utils/generalManipulations";
 import { getFormEndpoint } from "@components/utils/config";
 
-type ContactInfoType = {
-    name: string,
-    email: string,
-    message: string
-};
-
 export const Contact = () => {
-    const initialState = {
-        name: '',
-        email: '',
-        message: '',
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
+    const [canSubmitForm, setCanSubmitForm] = useState<boolean>(false);
+
+    const isNameFieldValid = (): boolean => {
+        return !(name.trim() === '');
     };
 
-    const [formValues, setFormValues] = useState<ContactInfoType>(initialState);
-    const [formErrors, setFormErrors] = useState<ContactInfoType>(initialState);
-
-    const nameValidation = (errors: ContactInfoType) => {
-        if (formValues.name.trim() === '') {
-            errors.name = 'Name is required';
-        }
+    const isEmailFieldValid = (): boolean => {
+        return !(email.trim() === '' || !isValidEmail(email.trim()));
     };
 
-    const emailValidation = (errors: ContactInfoType) => {
-        if (formValues.email.trim() === '') {
-            errors.email = 'Email is required';
-
-        } else if (!isValidEmail(formValues.email.trim())) {
-            errors.email = 'Invalid Email Format';
-        }
+    const isMessageFieldValid = () => {
+        return !(message.trim() === '');
     };
 
-    const messageValidation = (errors: ContactInfoType) => {
-        if (formValues.message.trim() === '') {
-            errors.message = 'Message is required';
-        }
+    const handleInputNameChange = (event) => {
+        setName(event.target.value);
     };
 
-    const areAnyErrors = (errors: ContactInfoType) => {
-        return errors.name !== '' || errors.email !== '' || errors.message !== '';
+    const handleInputEmailChange = (event) => {
+        setEmail(event.target.value);
     };
 
-    const handleSubmit = (event: React.FormEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        const errors = initialState;
-
-        nameValidation(errors);
-        emailValidation(errors);
-        messageValidation(errors);
-
-        if (areAnyErrors(errors)) {
-            setFormErrors(errors);
-            return;
-        }
+    const handleInputMessageChange = (event) => {
+        setMessage(event.target.value);
     };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-        const { name, value } = event.target;
-
-        setFormValues({
-            ...formValues,
-            [name]: value,
-        });
-
-        setFormErrors({
-            ...formErrors,
-            [name]: '',
-        });
-    };
+    useEffect(() => {
+        setCanSubmitForm(isNameFieldValid() && isEmailFieldValid() && isMessageFieldValid())
+    }, [name, email, message]);
 
     return (
-        <ContactContainerBox onSubmit={handleSubmit}>
+        <ContactContainerBox>
             {TextContainer('Contact', 'Get in Touch')}
             <SimpleText>Send me a note</SimpleText>
             <ContactContainerForm>
@@ -91,31 +56,27 @@ export const Contact = () => {
                         <ContactFormInput type={'text'}
                                           placeholder={'name'}
                                           name={'name'}
-                                          onChange={handleInputChange}
-                                          value={formValues.name}
+                                          onChange={handleInputNameChange}
+                                          value={name}
                         />
 
                         <ContactFormInput type={'email'}
                                           placeholder={'email'}
                                           name={'email'}
-                                          onChange={handleInputChange}
-                                          value={formValues.email}
+                                          onChange={handleInputEmailChange}
+                                          value={email}
                         />
                     </ContactEssentialFlex>
                     <ContactFormText>
                         <ContactFormTextArea
                             placeholder={'message'}
                             rows={6} name={'message'}
-                            onChange={handleInputChange}
-                            value={formValues.message}
+                            onChange={handleInputMessageChange}
+                            value={message}
                         />
                         <Separator paddingValue={0.6} />
-                        {formErrors.name && <ContactFormErrorMessage>{formErrors.name}</ContactFormErrorMessage>}
-                        <Separator paddingValue={0.6} />
-                        {formErrors.email && <ContactFormErrorMessage>{formErrors.email}</ContactFormErrorMessage>}
-                        <Separator paddingValue={0.6} />
-                        {formErrors.message && <ContactFormErrorMessage>{formErrors.message}</ContactFormErrorMessage>}
-                        <GeneralButton type={'submit'} whiteText={true}>
+
+                        <GeneralButton type={'submit'} whiteText={true} disabled={!canSubmitForm}>
                             Send message
                         </GeneralButton>
                     </ContactFormText>
