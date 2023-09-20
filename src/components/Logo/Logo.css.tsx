acb59657eebd1528d2a14969dfa42123145d72d6
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-09-20 16:59:35 +0300

    Update simple

--- src/components/Logo/Logo.css.tsx
+++ src/components/Logo/Logo.css.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const ImageWrapper = styled.div`
-    position: fixed;
     top: 0.5rem;
     left: 0.5rem;
 `;
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,40 +1,11 @@
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import RavenCodLogoWithMetadata from '../../assets/raven_cod_logo_white_metadata.png';
-import RavenCodLogo from '../../assets/raven_cod_logo_white.png';
 import { ImageWrapper } from "@components/components/Logo/Logo.css";
-import { useEffect, useState } from "react";
 
 export const Logo = () => {
-    const [scrollY, setScrollY] = useState<number>(0);
-    const [imageSrc, setImageSrc] = useState<StaticImageData>(RavenCodLogoWithMetadata);
-    const [imageWidth, setImageWidth] = useState<number>(150);
-
-    const threshold = 300;
-    const widthForLogoWithMetadata = 150;
-    const widthForLogoWithoutMetadata = 75;
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrollY(window.scrollY);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    useEffect(() => {
-        const isScreenScrolled = scrollY > threshold;
-
-        setImageSrc(isScreenScrolled ? RavenCodLogo : RavenCodLogoWithMetadata);
-        setImageWidth(isScreenScrolled ? widthForLogoWithoutMetadata : widthForLogoWithMetadata);
-    }, [scrollY]);
-
     return (
         <ImageWrapper>
-            <Image src={imageSrc} alt="Raven COD Logo" width={imageWidth} />
+            <Image src={RavenCodLogoWithMetadata} alt="Raven COD Logo" width={150} />
         </ImageWrapper>
     )
 }
