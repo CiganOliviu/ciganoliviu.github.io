b8d9585b99f517569e0bc841acb78fc40a8a2e46
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-12-30 11:51:21 +0200

    Updates

--- src/configs/projectsConfig.ts
+++ src/configs/projectsConfig.ts
@@ -1,10 +1,63 @@
-import VodBackground from "@components/assets/VodBackground.png";
-import SignalProcessing from "@components/assets/SignalProcessing.jpg";
-import BrainTumorDetection from "@components/assets/BrainTumorDetection.jpg";
-import CaRepair from "@components/assets/CaRepair.jpeg";
+import VodBackground from "@components/assets/projects/VodBackground.png";
+import BrainTumorDetection from "@components/assets/projects/BrainTumorDetection.jpg";
+import DesignPatterns from "@components/assets/projects/DesignPatterns.jpg";
+import CaRepair from "@components/assets/projects/CaRepair.jpeg";
 import { ProjectConfigType } from "@components/utils/types";
 
 export const ProjectsConfig: ProjectConfigType[] = [
+    {
+        thumbnail: BrainTumorDetection.src,
+        title: 'MedCity: Brain Tumor Detection System',
+        subtitle: 'An usecase driven approach',
+        previewHtmlField: {
+            __html: 'Brain-Tumor-Detection-System is a Proof of Concept (POC) project in the medical ' +
+                'field, designed to improve the early detection of brain tumors. It analyzes brain X-ray ' +
+                'images using advanced technology, providing high precision and real-time results, facilitating ' +
+                'rapid and accurate diagnosis.'
+        },
+        htmlField: {
+            __html: '',
+        },
+        open_link: '',
+        is_in_progress: true,
+        is_research_paper: false
+    },
+    {
+        thumbnail: CaRepair.src,
+        title: 'CaRepair: Car Damage Detector',
+        subtitle: 'An usecase driven approach',
+        previewHtmlField: {
+            __html: 'Employing state-of-the-art algorithms, this system processes vehicle images in real-time, ' +
+                'enabling high-precision analysis to swiftly categorize and evaluate damages. By leveraging ' +
+                'advanced technological capabilities, it aims to expedite the inspection process, providing' +
+                ' rapid and precise damage classification. This innovative solution seeks to streamline assessment' +
+                ' procedures, empowering automotive professionals with efficient tools for comprehensive and accurate ' +
+                'damage identification.'
+        },
+        htmlField: {
+            __html: '',
+        },
+        open_link: '',
+        is_in_progress: true,
+        is_research_paper: false,
+    },
+    {
+        thumbnail: DesignPatterns.src,
+        title: 'Software Design Patterns',
+        subtitle: 'An usecase driven approach',
+        previewHtmlField: {
+            __html: 'Brain-Tumor-Detection-System is a Proof of Concept (POC) project in the medical ' +
+                'field, designed to improve the early detection of brain tumors. It analyzes brain X-ray ' +
+                'images using advanced technology, providing high precision and real-time results, facilitating ' +
+                'rapid and accurate diagnosis.'
+        },
+        htmlField: {
+            __html: '',
+        },
+        open_link: '',
+        is_in_progress: true,
+        is_research_paper: false
+    },
     {
         thumbnail: VodBackground.src,
         title: 'StreamSnap VOD Mobile App',
@@ -48,74 +101,6 @@ export const ProjectsConfig: ProjectConfigType[] = [
                 ' not only enriched my technical expertise but also fostered critical thinking and problem-solving ' +
                 'abilities, preparing me for a wide range of challenges and opportunities in the field of engineering ' +
                 'and technology.',
-        },
-        open_link: '',
-        is_in_progress: true,
-        is_research_paper: false,
-    },
-    {
-        thumbnail: SignalProcessing.src,
-        title: 'Brushless DC electric motor Analysis',
-        subtitle: 'An usecase driven approach',
-        previewHtmlField: {
-            __html: 'This project is dedicated to achieving a comprehensive understanding of BLDC electric motors, ' +
-                'with the overarching goal of optimizing their efficiency and effectiveness across a broad range of industries.'
-        },
-        htmlField: {
-            __html: '<span style="font-size: 1.8rem">At the heart of this project</span> lies a fervent dedication to unraveling the intricate mechanisms' +
-                ' of BLDC (Brushless Direct Current) electric motors. With an overarching objective to fine-tune' +
-                ' their efficiency and efficacy across diverse industry landscapes, our endeavor encompasses a ' +
-                'comprehensive exploration into their operational nuances.' +
-                '<div>&nbsp;</div>' +
-                '<span style="font-size: 1.8rem">The project methodology unfolded</span>' +
-                ' through the application of two distinctive identification approaches: parametrical and ' +
-                'nonparametrical. Our primary aim revolved around an in-depth analysis of the outcomes derived ' +
-                'from both methodologies, seeking to discern the superior methodology yielding the most promising ' +
-                'results.' +
-                '<div>&nbsp;</div>' +
-                '<span style="font-size: 1.8rem">Embracing a multifaceted approach,</span> the project spans a ' +
-                'spectrum of activities encompassing' +
-                ' meticulous analysis, comprehensive graphical representations, and a profound exploration into the ' +
-                'core insights gleaned from these methodologies. From scrutinizing intricate data patterns to ' +
-                'crafting comprehensive visual representations, our mission is anchored in a holistic understanding ' +
-                'of BLDC electric motors, driving towards optimal efficiency and performance enhancements that ' +
-                'transcend industrial domains.',
-        },
-        open_link: 'https://github.com/CiganOliviu/brushless-DC-electric-motor-identification',
-        is_in_progress: false,
-        is_research_paper: false,
-    },
-    {
-        thumbnail: BrainTumorDetection.src,
-        title: 'MedCity: Brain Tumor Detection System',
-        subtitle: 'An usecase driven approach',
-        previewHtmlField: {
-            __html: 'Brain-Tumor-Detection-System is a Proof of Concept (POC) project in the medical ' +
-                'field, designed to improve the early detection of brain tumors. It analyzes brain X-ray ' +
-                'images using advanced technology, providing high precision and real-time results, facilitating ' +
-                'rapid and accurate diagnosis.'
-        },
-        htmlField: {
-            __html: '',
-        },
-        open_link: '',
-        is_in_progress: true,
-        is_research_paper: false
-    },
-    {
-        thumbnail: CaRepair.src,
-        title: 'CaRepair: Car Damage Detector',
-        subtitle: 'An usecase driven approach',
-        previewHtmlField: {
-            __html: 'Employing state-of-the-art algorithms, this system processes vehicle images in real-time, ' +
-                'enabling high-precision analysis to swiftly categorize and evaluate damages. By leveraging ' +
-                'advanced technological capabilities, it aims to expedite the inspection process, providing' +
-                ' rapid and precise damage classification. This innovative solution seeks to streamline assessment' +
-                ' procedures, empowering automotive professionals with efficient tools for comprehensive and accurate ' +
-                'damage identification.'
-        },
-        htmlField: {
-            __html: '',
         },
         open_link: '',
         is_in_progress: true,
