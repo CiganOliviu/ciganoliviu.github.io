3da4ea4c8e8aea9e07e664a5e01b006d646e0a4f
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2024-01-28 01:44:47 +0200

    Update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -54,3 +54,36 @@ export type ResumeConfigType = {
     education: EducationAndExperienceType[];
     experience: EducationAndExperienceType[];
 };
+
+export type HeroCardConfigType = {
+    WelcomeText: string;
+    TypeWriterContext: string[];
+    ContinuityText: string;
+    ButtonText: string;
+};
+
+export type KnowMeMoreConfigType = {
+    content: string;
+};
+
+type StandardContentType = {
+    icon: string;
+    title: string;
+    content: string;
+}
+
+export type WhatIDoConfigType = {
+    column_one: StandardContentType[];
+    column_two: StandardContentType[];
+};
+
+type SocialLinkType = {
+    url: string;
+    iconSource: string;
+}
+
+export type ExternalSocialLinksConfigType = {
+    links: SocialLinkType[];
+    target: string;
+    rel: string;
+};
