cd37844bfe17cb9628951714664905562c3a67b3
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2024-02-11 00:02:48 +0200

    Updates

--- src/components/ProjectsPreview/PreviewProjects.tsx
+++ src/components/ProjectsPreview/PreviewProjects.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from "react";
 import { ProjectsWrapper } from "@components/components/ProjectsPreview/Projects.css";
-import { PreviewProjectsConfig } from "@components/configs/projectsConfig";
+import { ProjectsConfig } from "@components/configs/projectsConfig";
 import { Separator } from "@components/components/Resume/Resume.css";
 import { ContentLinkWrapper } from "@components/utils/DetailPageStyles.css";
 import Link from "next/link";
 import { AppTheme } from "@components/utils/cssMedia";
 import { ProjectsLister } from "@components/components/ProjectsLister/ProjectsLister";
+import { ProjectConfigType } from "@components/utils/types";
+import { useGetScreenSize } from "@components/hooks/useScreenSize";
 
 export const PreviewProjects: FC = () => {
+    const { isBigScreen } = useGetScreenSize();
+
+    const PreviewProjectsConfig: ProjectConfigType[] = ProjectsConfig.slice(0, isBigScreen() ? 3 : 2);
+
     return (
         <ProjectsWrapper>
             <ProjectsLister projectsList={PreviewProjectsConfig} />
