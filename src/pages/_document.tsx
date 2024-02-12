1378d78187f40426aed14143e2f3d51d199da59c
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2024-02-12 13:10:25 +0200

    Revert

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,25 +4,7 @@ import { ServerStyleSheet } from 'styled-components';
 const MyDocument = () => {
     return (
         <Html lang="en">
-            <Head>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                              (function(l) {
-                                    if (l.search[1] === '/' ) {
-                                    var decoded = l.search.slice(1).split('&').map(function(s) { 
-                                        return s.replace(/~and~/g, '&')
-                                    }).join('?');
-                                    
-                                    window.history.replaceState(null, null,
-                                      l.pathname.slice(0, -1) + decoded + l.hash
-                                    );
-                                }
-                              }(window.location))
-                        `,
-                    }}
-                ></script>
-            </Head>
+            <Head />
             <body>
             <Main />
             <NextScript />
