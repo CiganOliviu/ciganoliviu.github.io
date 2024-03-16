5acc235c3a5c1c324c1c233e8500acdd08e6a276
CiganOliviu <ciganoliviudavid@gmail.com>
2024-03-16 22:37:30 +0200

    Deploy work-ethic

--- src/configs/articlesConfig.ts
+++ src/configs/articlesConfig.ts
@@ -11,8 +11,66 @@ import HowToConductCodeReviews from "@components/assets/articles/how_to_conduct_
 import HowToBuildASeniorDeveloperMindset from "@components/assets/articles/how_to_build_a_senior_developer_mindset.jpg";
 import ChoosingTheRightEstimatorInMachineLearning from "@components/assets/articles/choosing_the_right_estimator_in_machine_learning.jpg"
 import HowBusinessKnowledgeCanIncreaseYourValueAsProgrammer from "@components/assets/articles/how_business_knowledge_can_increase_your_value_as_programmer.webp";
+import WhatIsWorkEthic from "@components/assets/articles/what_is_work_ethic.jpg";
 
 export const ArticlesConfig: ArticlesConfigType[] = [
+    {
+        id: 'what-is-work-ethic',
+        thumbnail: WhatIsWorkEthic.src,
+        title: 'What is work ethic',
+        subtitle: '12 March 2024',
+        previewField: 'Work ethic, like many other parts of life, is highly subjective and differs from person to ' +
+            'person. With that said, I’d like to share my understanding of what work ethic entails. But first, ' +
+            'let’s define what work ethic means....',
+        htmlField: {
+            __html: 'Work ethic, like many other parts of life, is highly subjective and differs from person to person. With that said, I’d like to share my understanding of what work ethic entails. But first, let’s define what work ethic means.' +
+                '<div>&nbsp;</div>' +
+                'According to chatGPT => “Work ethic refers to a set of values, principles, and beliefs that influence how an individual approaches their work and professional responsibilities. It encompasses traits such as diligence, reliability, punctuality, discipline, dedication, integrity, and a commitment to excellence. Someone with a strong work ethic typically demonstrates a willingness to put in the necessary effort, time, and energy to complete tasks efficiently and effectively, often going above and beyond what is required. Additionally, individuals with a strong work ethic often take pride in their work and strive for continuous improvement and growth in their professional endeavors”' +
+                '<div>&nbsp;</div>' +
+                'Basically, you can define what work ethic means to you based on your goals and perspectives. There isn’t a clear definition of what work ethic means because it varies a lot. It’s about your moral values and what you believe is best. If you believe working four hours every single day or 8 hours from Monday to Friday is enough, then that is your work ethic, and that is respected in today’s society, working less. Often people who work more are tagged as workaholics.' +
+                '<div>&nbsp;</div>' +
+                '<h1 style="line-height: 3rem">My definition of work ethic</h1>' +
+                '<div>&nbsp;</div>' +
+                'To me, work ethic isn’t just about punching in and out of the office; it’s about maximizing productivity and efficiency. It’s about getting a lot done in the time you have, whether it’s a regular workday or a weekend. This means putting in those long hours, focusing on action rather than just dreaming about it.' +
+                '<div>&nbsp;</div>' +
+                'I don’t believe in the old debate of working hard versus working smart; it’s about combining the two. Working smart means finding the most effective ways to accomplish tasks, while working hard means putting in the effort to see those tasks through to completion. It’s a balance, a synergy between hustle and strategy.' +
+                '<div>&nbsp;</div>' +
+                'For me, part of maintaining a strong work ethic is about discipline — being able to resist the temptations of modern distractions like social media and video games. I limit my social media use to just five minutes a day, and I’ve cut out PC games entirely. It’s not about depriving myself of fun; it’s about prioritizing my time and energy toward what truly matters to me: my work and my goals.' +
+                '<div>&nbsp;</div>' +
+                'Speaking of goals, I don’t confine my work ethic to just weekdays. Weekends are working days too. A successful day for me means putting in a solid 12 hours at the office. If I’ve clocked less than that, I start questioning how I’ve managed my time.' +
+                '<div>&nbsp;</div>' +
+                'But let’s not forget the importance of self-care. Working hard is great, but it’s not sustainable without taking care of yourself. That means hitting the gym regularly, getting enough sleep (7–8 hours for me), and spending quality time with family and loved ones. After all, what good is professional success if it comes at the cost of your health and relationships?' +
+                '<div>&nbsp;</div>' +
+                'So, that’s my philosophy on work ethic — a blend of dedication, efficiency, and discipline. It’s about putting in the work, working smart, and taking care of yourself along the way.' +
+                '<div>&nbsp;</div>' +
+                '<h1 style="line-height: 3rem">The problem I see</h1>' +
+                '<div>&nbsp;</div>' +
+                'You know how everyone’s always saying they don’t have enough time? It’s like a broken record! But honestly, when someone tells me that, I can’t help but think a few things about them. First off, they probably stink at managing their time. It’s like they haven’t figured out how to juggle all the things they need to do. Maybe they spend too much time on things that aren’t really important in the grand scheme of things, like endlessly scrolling through social media or getting sucked into watching cat videos.' +
+                '<div>&nbsp;</div>' +
+                'Then there’s the whole issue of not knowing what’s important and what can wait. Like, sure, binge-watching that new show might be fun, but does it really need to come before hitting the gym or finishing that work project? Prioritizing seems to be a struggle for a lot of folks. It’s like they can’t differentiate between what’s urgent and what’s just a distraction.' +
+                '<div>&nbsp;</div>' +
+                'And let’s not forget about all that wasted time on stuff that doesn’t really matter. Like, seriously, how many hours do you spend scrolling mindlessly through social media or getting lost in a YouTube rabbit hole? It’s crazy when you think about it!' +
+                '<div>&nbsp;</div>' +
+                'I always tell people to quit with the “I don’t have enough time” excuse because it just makes them sound clueless. It’s not about having too little time; it’s about not knowing how to use the time you’ve got wisely. We’ve all got the same 24 hours in a day; it’s what you do with them that counts.' +
+                '<div>&nbsp;</div>' +
+                '<h1 style="line-height: 3rem">Prioritization issues</h1>' +
+                '<div>&nbsp;</div>' +
+                '<i>“Did you look over that certain issue we discussed? It’s important.” — “No, I didn’t have time.”</i>' +
+                '<div>&nbsp;</div>' +
+                'Well, no wonder you didn’t have time if you’re spending hours scrolling through social media every day. And if you’re prioritizing 9 to 10 hours of sleep every night, or hanging out at cafes with friends who aren’t exactly motivating you to reach your goals, then yeah, time might be a bit tight.' +
+                '<div>&nbsp;</div>' +
+                'It’s kind of ironic when you think about it, right? Many complain about not having enough time, but then they waste it on stuff that doesn’t really matter. I always try to remind people to start prioritizing activities that actually make a difference in their lives. Sitting on social media, sleeping in too much, or going out every night won’t get you anywhere. So, using the “not enough time” excuse just doesn’t cut it when you’re spending your time on things that give you zero return on investment.' +
+                '<div>&nbsp;</div>' +
+                '<h1 style="line-height: 3rem">Cut the fat</h1>' +
+                '<div>&nbsp;</div>' +
+                'In my opinion, anything that doesn’t directly or indirectly contribute to building yourself and your brand should be completely cut from your schedule. Stop wasting time on things that don’t matter, and I guarantee you’ll find yourself with more than enough time to focus on what truly counts. All your energy should be directed towards three core categories: health, wealth, and love. Social media, watching TV channels, or hanging out in cafes with negative influences don’t belong anywhere near these essential pillars of life.' +
+                '<div>&nbsp;</div>' +
+                'In conclusion, it’s crucial to master time management and work efficiently. Let’s ditch the habit of randomly saying “I don’t have enough time” because deep down, you know it’s often an excuse. Playing the victim won’t get you anywhere; it’s time to step up and take control. If you want to see real progress, it’s essential to change your mindset and approach. However, if you are content with staying stagnant and making little to no progress, then by all means, keep using that excuse. But let’s challenge ourselves to think differently and aim for better outcomes.' +
+                '<div>&nbsp;</div>' +
+                'Thank you.'
+        },
+        open_link: 'https://medium.com/@ciganoliviudavid/what-is-work-ethic-7128ebdaa104'
+    },
     {
         id: 'how-business-knowledge-can-increase-your-value-as-programmer',
         thumbnail: HowBusinessKnowledgeCanIncreaseYourValueAsProgrammer.src,
