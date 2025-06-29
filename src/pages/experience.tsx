2d13aed266286c11f416d661737bd8a7fd2db9e1
CiganOliviuDavid <ciganoliviudavid@gmail.com>
2025-06-29 23:18:01 +0300

    try fix 2.0

--- src/pages/experience.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { RenderExperiencePage } from "@components/components/RenderExperiencePage/RenderExperiencePage";
-
-const experience = () => {
-    return <RenderExperiencePage />;
-};
-
-export default experience;
